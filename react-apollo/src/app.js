5a3391ade3c90f3a1aede86791f9f6e0bf2cf7bb
Marc Pomar <marc@faable.com>
2020-04-13 20:19:27 +0200

    graph

---
+++ react-apollo/src/app.js
@@ -0,0 +1,11 @@
+const { ApolloServer, gql } = require("apollo-server");
+import { typeDefs, resolvers } from "./schema";
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ react-apollo/src/schema.js
@@ -0,0 +1,38 @@
+const { makeExecutableSchema } = require("graphql-tools");
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: {
+      name: "J.K.",
+      surname: " Rowling",
+      emoji: "Hola",
+    },
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton",
+  },
+];
+
+// The GraphQL schema in string form
+export const typeDefs = `
+  type Query { books: [Book], todosEnUno:String }
+  type Book { title: String, author: Author }
+  type Author { name: String, surname: String, emoji:Emoji }
+  type Emoji { miemoji: String }
+`;
+
+// The resolvers
+export const resolvers = {
+  Query: {
+    books: () => books,
+    todosEnUno: () => {
+      return books.map((book) => book.title).join(",");
+    },
+  },
+  Emoji: () => {
+    console.log("Nested resolver");
+    return { miemoji: "👨🏼‍💻" };
+  },
+};
